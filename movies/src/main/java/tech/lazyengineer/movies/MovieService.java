0edd0d0b8ff711d7b65edb14734a2988ad1e4033
Mukesh Suthar <49620706+mukeshs1510@users.noreply.github.com>
2023-07-26 16:54:20 +0530

    Added get movies end point

---
+++ movies/src/main/java/tech/lazyengineer/movies/MovieService.java
@@ -0,0 +1,15 @@
+package tech.lazyengineer.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovie() {
+        return movieRepository.findAll();
+    }
+}
